a63e73064800d36961b8239580046c3a4a13ab55
Tarun Gusain <36428256+Ishhyoboytarun@users.noreply.github.com>
2023-10-17 19:25:30 +0530

    Update models.go

--- models/models.go
+++ models/models.go
@@ -2,7 +2,7 @@ package models
 
 // User schema of the user table
 type Stock struct {
-	StockID int64  `json:"stockid"`
+	StockID int64  `json:"stockId"`
 	Name    string `json:"name"`
 	Price   int64  `json:"price"`
 	Company string `json:"company"`
